65156d87017accd0134264ad0295e2fce668a5c5
Volen Angelov <volen.angelov@gmail.com>
2025-03-17 16:22:58 +0000

    Fix errors

--- src/components/Qwerty.tsx
+++ src/components/Qwerty.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { observer } from "mobx-react-lite";
 
 interface GameStore {
@@ -29,14 +30,14 @@ export default observer(function Qwerty({ store }: QwertyProps) {
     };
 
     return (
-        <div>
+        <div className="w-full sm:w-auto">
             {qwerty.map((row) => (
-                <div className="flex justify-center gap-x-1 mt-1" key={row}>
+                <div className="flex grow w-full justify-center gap-x-1 mt-1" key={row}>
                     {row.split('').map((key, index) => {
                         if (key === 'D' && row.startsWith('Delete')) {
                             return (
                                 <button
-                                    className="cursor-pointer m-px px-1 text-black flex h-10 w-20 items-center justify-center rounded-md bg-gray-300 uppercase"
+                                    className="cursor-pointer m-px px-1 text-black flex h-8 sm:h-10 w-20 text-xs sm:text-md items-center justify-center rounded-md bg-gray-300 uppercase"
                                     key="delete"
                                     onClick={() =>
                                         store.handleKeyup({ key: 'Backspace' } as KeyboardEvent)
@@ -49,7 +50,7 @@ export default observer(function Qwerty({ store }: QwertyProps) {
                         if (key === 'E' && row.endsWith('Enter')) {
                             return (
                                 <button
-                                    className="cursor-pointer m-px px-1 text-black flex h-10 w-20 items-center justify-center rounded-md bg-gray-300 uppercase"
+                                    className="cursor-pointer m-px px-1 text-black flex text-xs sm:text-md h-8 sm:h-10 w-20 items-center justify-center rounded-md bg-gray-300 uppercase"
                                     key="enter"
                                     onClick={() => {
                                         store.submitGuess();
@@ -76,7 +77,7 @@ export default observer(function Qwerty({ store }: QwertyProps) {
                             : 'bg-gray-200';
                         return (
                             <button
-                                className={`cursor-pointer m-px text-black flex h-10 w-10 items-center justify-center rounded-md uppercase ${bgColor}`}
+                                className={`cursor-pointer m-px text-black flex h-8 w-full sm:h-10 sm:w-10 items-center text-md justify-center rounded-md uppercase ${bgColor}`}
                                 key={key}
                                 onClick={() => handleLetterClick(key)}
                             >
